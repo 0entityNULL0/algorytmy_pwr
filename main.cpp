e99d62335dc38f1454a0c5ea700c0d27c1ffd308
example <example>
2026-03-24 23:22:07 +0100

    babelkowe i wstawianie

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include "sources/sortowanie_b.h"
+
+int czy_wieksze(int a,int b)
+{
+	if(a>b) return 1;
+	if(a<b) return -1;
+	return 0;
+}
 
 int main()
 {
-		std::cout<<"helo wort\n";
-		return 0;
+	
+	int a[5]={3,2,7,4,5};
+	for(int i=0;i<5; i++)
+		std::cout<<a[i]<<' ';
+	std::cout<<'\n';
+	wstaw(a,5,czy_wieksze);
+	for(int i=0;i<5; i++)
+		std::cout<<a[i]<<' ';
+	std::cout<<'\n';
+	return 0;
 }
